0e85ee82eb02d44d5d9310b428137f42d8200185
RaffaeleCorso <exta9ts@posteitaliane.it>
2026-03-25 18:39:42 +0100

    Fix get one

--- lezione_7/mio-primo-laravel/routes/api.php
+++ lezione_7/mio-primo-laravel/routes/api.php
@@ -4,4 +4,8 @@ use App\Http\Controllers\Api\ArticoloApiController;
 use Illuminate\Support\Facades\Route;
 
 // Genera automaticamente i 5 endpoint RESTful standard!
-Route::apiResource('articoli', ArticoloApiController::class);
+//Route::apiResource('articoli', ArticoloApiController::class);
+
+Route::apiResource('articoli', ArticoloApiController::class)->parameters([
+    'articoli' => 'articolo' // Insegniamo a Laravel il singolare!
+]);
